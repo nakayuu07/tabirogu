28d8b3e054b8f9175c027fb575eb61233cab89b1
yuya nakamura <nakayuu07@gmail.com>
2018-04-16 18:51:49 +0900

    introduce SENDGRID

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,6 +92,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #remember to change localhost to actual host
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: '自分のHerokuアプリのドメイン' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings =
+  {
+   user_name: ENV['SENDGRID_USERNAME'],
+   password: ENV['SENDGRID_PASSWORD'],
+   domain: "heroku.com",
+   address: "smtp.sendgrid.net",
+   port: 587,
+   authentication: :plain,
+   enable_starttls_auto: true
+  }
 end
